66c5ff9051f139dcccdf41cea1361e3c2a6b8ea6
kumar-praveens <praveenkumars2831@gmail.com>
2022-05-26 08:29:38 +0530

    enabling cors

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const toursRouter = require("./routes/toursRoute");
 const techRouter = require("./routes/technologyRoute");
 const fitnessRoute = require("./routes/fitnessRoute");
 const articleRoute = require("./routes/articleRoute");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
